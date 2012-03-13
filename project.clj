1f36b2091e0a5c68ce4caed0e29e036cfa5880ee
Drew Raines <drew@raines.me>
2012-03-13 09:37:17 -0500

    clj-http 0.3.3. Bump to 2.2.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject httprof "2.0"
+(defproject httprof "2.2"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clj-http "0.2.7"]
+                 [clj-http "0.3.3"]
                  [org.clojure.contrib/repl-utils "1.3.0-alpha4"]]
   :dev-dependencies [[ring/ring-jetty-adapter "1.0.2"]]
   :main httprof.core)
