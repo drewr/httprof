827f4c800c6ae553a041c345785e0cee9fc3a7e8
Drew Raines <aaraines@gmail.com>
2012-08-13 16:16:04 -0500

    Update deps.

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject httprof "2.2"
+(defproject httprof "2.3"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clj-http "0.3.3"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-http "0.5.2"]
                  [org.clojure.contrib/repl-utils "1.3.0-alpha4"]]
-  :dev-dependencies [[ring/ring-jetty-adapter "1.0.2"]]
   :main httprof.core)
